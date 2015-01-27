2f2a8567e6ca52301e2ae2fa1f3168f1649fa39d
Brian Mearns <bmearns@ieee.org>
2015-01-26 22:14:04 -0500

    Added a Length base class.

--- pyps/geom/__init__.py
+++ pyps/geom/__init__.py
@@ -5,6 +5,10 @@ import abc
 import collections
 
 class Point(object):
+    """
+    Abstract class provides the interface for all points.
+    """
+
     __metaclass__ = abc.ABCMeta
 
     @staticmethod
@@ -58,9 +62,19 @@ class Point(object):
     def translate(self, dx=0, dy=0):
         return Translation(self, dx, dy)
 
+    def pin(self):
+        """
+        Creates and returns an instance of `Pt` at the current location of this
+        point.
+        """
+        return Pt(*(self.coords()))
 
 
 class Pt(Point):
+    """
+    Represents a `Point` at a fixed location.
+    """
+
     def __init__(self, x, y):
         self.__x = x
         self.__y = y
@@ -76,6 +90,36 @@ class Pt(Point):
     @property
     def y(self):
         return self.__y
+
+
+class Length(object):
+    """
+    Represents a length.
+    """
+
+    __metaclass__ = abc.ABCMeta
+
+    @staticmethod
+    def cast(self, other):
+        if isinstance(other, Length):
+            return other
+        elif isinstance(other, (float, int, long)):
+            return FixedLength(other)
+        raise TypeError('Could not cast to a Length: %r' % (other,))
+
+    @abc.abstractmethod
+    def __float__(self):
+        raise NotImplementedError()
+
+    def __str__(self):
+        return "%s" % (float(self),)
+        
+class FixedLength(Length):
+    def __init__(self, length):
+        self.__length = float(length)
+
+    def __float__(self):
+        return self.__length
 
 
 class Translation(Point):
