3fb907c0eba182080d4727a4985042fa31f2d065
Brian Mearns <bmearns@ieee.org>
2015-01-30 22:08:25 -0500

    Demo acceptance criteria for SHP3 userstory.
    a) User is able to specify color of the fill in RGB8
    b) User is able to specify no fill for their shapes
    c) Shape is visible beneath a shape with no fill.
    
    Draws two circles, the one behind has a fill specified in RGB8, the
    second is above and has no fill.

--- pyps/__main__.py
+++ pyps/__main__.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     doc = pyps.Document()
 
     doc.add_shape(Circle((100, 100), 50, title="My Circle", fill=(255, 128, 50)))
-    doc.add_shape(Circle((180, 100), 30, title="My Circle"))
+    doc.add_shape(Circle((150, 100), 30, title="My Circle"))
 
     writer = EPSWriter()
     with open('test_output.eps', 'wb') as ostream:
